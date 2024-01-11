94d3a5f483e49191cb6d56472f59ff0bb4041747
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-01-11 13:10:04 +0530

    First Twitter codes

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const connect = require('./config/database');
+const app = express();
+
+const TweetRepository = require('./repository/tweet-repository');
+const Comment = require('./models/comment');
+const Tweet = require('./models/tweet');
+
+app.listen(3000, async() => {
+    console.log('Server started');
+    await connect();
+    console.log('Mongo db connected');
+
+    // const tweet = await Tweet.create({
+    //     content: 'Third Tweet',
+    // });
+    // const tweets = await Tweet.find({userEmail: 'a@b.com'});
+    // const tweet = await Tweet.findById('659ef4a1fe61fba13da2fe64');
+    // tweet.userEmail = 'b@c.com';
+    // await tweet.save();
+
+    const tweetRepo = new TweetRepository();
+    // const tweet = await tweetRepo.update('659efaaa0a04b9ea9f6f0ab8'), 
+    // {content: 'New tweet brewing'});
+    // const tweet = await tweetRepo.create({content: 'Tweet with comment schema'});
+    // console.log(tweet);
+    // const comment = await Comment.create({content: 'New comment'});
+    // tweet.comments.push(comment);
+    // await tweet.save();
+
+    const tweet = await tweetRepo.getWithComments('659f8d97b436a315a1dcb36d');
+    console.log(tweet);
+});
