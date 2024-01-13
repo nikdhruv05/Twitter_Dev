7b7a5e779077269d2899e9c1db8f122cc4bde78b
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-01-13 17:48:41 +0530

    Added v1 for tweet model and hashtag model

--- src/index.js
+++ src/index.js
@@ -10,28 +10,5 @@ app.listen(3000, async() => {
     console.log('Server started');
     await connect();
     console.log('Mongo db connected');
-
-    // const tweet = await Tweet.create({
-    //     content: 'Third Tweet',
-    // });
-    // const tweets = await Tweet.find({userEmail: 'a@b.com'});
-    // const tweet = await Tweet.findById('659ef4a1fe61fba13da2fe64');
-    // tweet.userEmail = 'b@c.com';
-    // await tweet.save();
-
-    const tweetRepo = new TweetRepository();
-    // const tweet = await tweetRepo.update('659efaaa0a04b9ea9f6f0ab8'), 
-    // {content: 'New tweet brewing'});
-    // const tweet = await tweetRepo.create({content: 'Tweet with comment schema'});
-    // console.log(tweet);
-    // const comment = await Comment.create({content: 'New comment'});
-    // tweet.comments.push(comment);
-    // await tweet.save();
-
-    // const tweet = await tweetRepo.getWithComments('659f8d97b436a315a1dcb36d');
-    // const tweet = await tweetRepo.getAll(0, 4);
-    // console.log(tweet[0].contentWithEmail);
-
-    const tweet = await tweetRepo.create({content: 'With hooks'});
-    console.log(tweet);
+    
 });
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -4,27 +4,16 @@ const tweetSchema = new mongoose.Schema({
     content: {
         type:String,
         required: true,
+        max: [200, 'Tweet can not be more than 250 characters'],
     },
-    userEmail: {
-        type: String
-    },
-    comments: [
+    hashtags: [
         {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
+            ref: 'Hashtag',
         }
-    ],
+    ]
 }, {timestamps: true});
 
-tweetSchema.virtual('contentWithEmail').get(function process() {
-    return `${this.content}\nCreated by: ${this.userEmail}`;
-});
-
-tweetSchema.pre('save', function(next) {
-    console.log('Inside a hook');
-    this.content = this.content + '....';
-    next();
-});
 
 const Tweet = mongoose.model('Tweet', tweetSchema);
 module.exports = Tweet;
