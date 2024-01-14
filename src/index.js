f8c29fe05ef632444d3949f69eb56687a065cb09
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-01-15 01:00:53 +0530

    Added tweet service with hashtag

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comment');
-const Tweet = require('./models/tweet');
-
 app.listen(3000, async() => {
     console.log('Server started');
     await connect();
     console.log('Mongo db connected');
-    
-});
+}); 
