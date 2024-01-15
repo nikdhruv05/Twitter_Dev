1990f21f5fa3d34cf634ef22955c2ef523021cea
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-01-15 20:05:11 +0530

    Added like model and basic user model

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const User = new mongoose.Schema('User', userSchema);
+
+export default User;
