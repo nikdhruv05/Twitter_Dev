c8a425e02b8fc7dc87ecec4de94ff70cc5554469
Dhruv Gupta <kumardhruv05112002@gmail.com>
2024-01-18 02:02:34 +0530

    Added passport logic

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -9,6 +9,37 @@ class UserService {
         const user = await this.userRepository.create(data);
         return user;
     }
+
+    async getUserByEmail(email) {
+        try {
+            const user = await this.userRepository.findBy({email});
+            return user;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async signin(data) {
+        try {
+            const user = await this.getUserByEmail(data.email);
+            if(!user){
+                throw { 
+                    message: 'no user found',
+                    success: false,
+                };
+            }
+            if(!user.comparePassword(data.password)){
+                throw {
+                    message: 'incorrect password',
+                    success: false,
+                };
+            }
+            const token = user.genJWT();
+            return token;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 
 export default UserService;
